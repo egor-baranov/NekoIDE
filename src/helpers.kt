0d67199010e80dc0c614eb882b9e317773defd5e
egorbaranov <enterskylife@gmail.com>
2020-01-21 21:49:38 +0300

    :partying_face: Сделал покраску текста: программа тречит изменения в текстовом редакторе, проходится по словам, выделяет служебные и числа

--- src/helpers.kt
+++ src/helpers.kt
@@ -1,9 +1,11 @@
+import java.lang.Exception
+
 fun lStrip(input: String): String {
     var left: Int = 0
     for (i in input) {
         if (i in arrayListOf<Char>('\n', '\r', '\t', ' '))
             ++left
-        else break;
+        else break
     }
     var ret: String = ""
     for (i in left until input.length)
@@ -13,5 +15,81 @@ fun lStrip(input: String): String {
 
 fun rStrip(input: String): String {
     var right: Int = 0
-    return ""
+    for (i in input.length - 1..0) {
+        if (input[i] in arrayListOf<Char>('\n', '\r', '\t', ' '))
+            ++right
+        else break
+    }
+    var ret: String = ""
+    for (i in 0 until input.length - right)
+        ret += input[i]
+    return ret
+}
+
+fun strip(input: String): String {
+    return lStrip(rStrip(input))
+}
+
+fun unite(s1: Set<String>, s2: Set<String>): Set<String> {
+    val ret: MutableSet<String> = mutableSetOf()
+    for (i in s1) ret.add(i)
+    for (i in s2) ret.add(i)
+    return ret
+}
+
+fun toString(input: Char): String = input.toString()
+
+fun toString(input: ArrayList<Word>): String {
+    var ret = "["
+    for (i in 0 until input.size) {
+        ret += "{${input[i].source}, ${input[i].index}}"
+        if (i != input.size - 1) ret += ", "
+    }
+    ret += "]"
+    return ret
+}
+
+class Word(s: String, i: Int) {
+    val source = s
+    val index = i
+}
+
+fun split(input: String, sep: Set<Char>): ArrayList<Word> {
+    val ret = arrayListOf<Word>()
+    var tmp: String = ""
+    for (i in input.indices) {
+        if (sep.contains(input[i])) {
+            if (tmp.isNotEmpty())
+                ret.add(Word(tmp, i - tmp.length))
+            tmp = ""
+            continue
+        }
+        tmp += input[i]
+    }
+    if (tmp.isNotEmpty())
+        ret.add(Word(tmp, input.length - tmp.length))
+    return ret;
+}
+
+fun isNumber(input: String): Boolean {
+    if (input[0] > '9' || input[0] < '0') return false
+    var dots: Int = 0
+    for (i in 1 until input.length - 1) {
+        if (input[i] == '.') {
+            ++dots
+            continue
+        }
+        if (input[i] > '9' || input[i] < '0') return false
+    }
+    if (input[input.length - 1] > '9' || input[input.length - 1] < '0') return false
+    return dots <= 1
+}
+
+fun cleared(input: String): String {
+    var ret = ""
+    for (i in input) {
+        if (i == '\r')  continue
+        ret += i
+    }
+    return ret
 }
--- src/main.kt
+++ src/main.kt
@@ -1,58 +1,55 @@
 import java.awt.Color
 import java.awt.EventQueue
+import java.awt.Font
 import javax.swing.*
+import javax.swing.event.DocumentEvent
+import javax.swing.event.DocumentListener
 import javax.swing.text.SimpleAttributeSet
 import javax.swing.text.Style
 import javax.swing.text.StyleConstants
 
-val darculaBackground = Color(43, 43, 43)
-val darculaOrange = Color(203, 96, 45)
+
+val darculaBackgroundColor = Color(43, 43, 43)
+val darculaOrange = Color(204, 120, 50)
+val textSelectionColor = Color(42, 80, 142)
+val darculaPurple = Color(152, 118, 170)
+val numberBlue = Color(104, 151, 187)
+val commentGrey = Color(128, 128, 128)
+val textColor = Color(169, 183, 198)
+val stringGreen = Color(106, 135, 89)
+val badCharacterRed = Color(255, 0, 0)
 
 class JTextPaneTest : JFrame("Neko IDE") {
 
     var textEditor: JTextPane? = null
 
-    private var heading: Style? = null
     private var normal: Style? = null
-    private val STYLE_heading = "heading"
     private val STYLE_normal = "normal"
     private val FONT_style = "Fira Code"
     private val TEXT = arrayOf(
-        arrayOf("Компонент JTextPane \r\n", "heading"),
-        arrayOf("\r\n", "normal"),
-        arrayOf("JTextPane незаменим при создании в приложении  \r\n", "normal"),
-        arrayOf("многофункционального текстового редактора.\r\n", "normal"),
-        arrayOf("\r\n", "normal"),
-        arrayOf("Он позволяет вставлять в документ визуальные \r\n", "normal"),
-        arrayOf("компоненты типа JCheckBox и JRadioButton.\r\n ", "normal")
+        arrayOf("Компонент or ", "normal"),
+        arrayOf("", "normal"),
+        arrayOf("JTextPane незаменим при создании в приложении ", "normal"),
+        arrayOf("многофункционального public редактора.", "normal"),
+        arrayOf("", "normal"),
+        arrayOf("Он and вставлять в документ визуальные ", "normal"),
+        arrayOf("компоненты типа fun и JRadioButton. ", "normal")
     )
 
     private fun createStyles(editor: JTextPane) { // Создание стилей
-        normal = editor.addStyle(STYLE_normal, null)
-        StyleConstants.setFontFamily(normal, FONT_style)
-        StyleConstants.setFontSize(normal, 12)
-        // Наследуем свойстdо FontFamily
-        heading = editor.addStyle(STYLE_heading, normal)
-        StyleConstants.setFontSize(heading, 24)
-        StyleConstants.setBold(heading, true)
-        StyleConstants.setForeground(normal, Color.white)
+//        normal = editor.addStyle(STYLE_normal, null)
+//        StyleConstants.setFontFamily(normal, FONT_style)
+//        StyleConstants.setFontSize(normal, 14)
+//        // Наследуем свойство FontFamily
+//        StyleConstants.setForeground(normal, textColor)
     }
 
     private fun loadText(editor: JTextPane) { // Загружаем в документ содержимое
-        for (i in TEXT.indices) {
-            val style = if (TEXT[i][1] == STYLE_heading) heading else normal
-            insertText(editor, TEXT[i][0], style)
-        }
+        for (i in TEXT.indices)
+            insertText(editor, TEXT[i][0], normal)
         // Размещение компонента в конце текста
         val doc = editor.styledDocument
         editor.caretPosition = doc.length
-    }
-
-    private fun changeDocumentStyle(editor: JTextPane) { // Изменение стиля части текста
-        val orange = SimpleAttributeSet()
-        StyleConstants.setForeground(orange, darculaOrange)
-        val doc = editor.styledDocument
-        doc.setCharacterAttributes(10, 9, orange, false)
     }
 
     private fun insertText(editor: JTextPane, string: String, style: Style?) {
@@ -72,7 +69,7 @@ class JTextPaneTest : JFrame("Neko IDE") {
         createStyles(textEditor!!)
         // Загрузка документа
         loadText(textEditor!!)
-        changeDocumentStyle(textEditor!!)
+        // changeDocumentStyle(textEditor!!)
         // Размещение редактора в панели содержимого
         contentPane.add(JScrollPane(textEditor))
         // Открытие окна
@@ -87,10 +84,58 @@ class JTextPaneTest : JFrame("Neko IDE") {
     }
 }
 
+fun paintEditor(textEditor: JTextPane) {
+
+    val editorSD = textEditor.styledDocument
+
+    fun paintWord(word: Word, clr: Color) {
+        val tmpAS = SimpleAttributeSet()
+        StyleConstants.setForeground(tmpAS, clr)
+
+        val paintRunnable = Runnable {
+            editorSD.setCharacterAttributes(word.index, word.source.length, tmpAS, false)
+        }
+        SwingUtilities.invokeLater(paintRunnable)
+    }
+
+    paintWord(Word(textEditor.text, 0), textColor)
+
+    for (i in split(cleared(textEditor.text), setOf(' ', '\n', '\r'))) {
+        if (Keywords.contains(i.source) or Constants.contains(i.source))
+            paintWord(i, darculaOrange)
+        if (isNumber(i.source)) paintWord(i, numberBlue)
+    }
+
+    println(textEditor.text)
+}
+
+class EditorListener(t: JTextPane) : DocumentListener {
+
+    var textEditor: JTextPane = t
+
+    override fun insertUpdate(e: DocumentEvent) {
+        paintEditor(textEditor)
+    }
+
+    override fun removeUpdate(e: DocumentEvent) {
+        paintEditor(textEditor)
+    }
+
+    override fun changedUpdate(e: DocumentEvent?) { //Plain text components do not fire these events
+        // paintEditor(textEditor)
+    }
+}
+
 private fun createAndShowGUI() {
     val frame = JTextPaneTest()
-    frame.textEditor!!.background = darculaBackground
+
+    frame.textEditor!!.font = Font("Fira Code", Font.PLAIN, 14)
+
+    frame.textEditor!!.background = darculaBackgroundColor
     frame.textEditor!!.caretColor = Color.white
+    frame.textEditor!!.selectionColor = textSelectionColor
+    frame.textEditor!!.document.addDocumentListener(EditorListener(frame.textEditor!!))
+    paintEditor(frame.textEditor!!)
 }
 
 fun main(args: Array<String>) {
